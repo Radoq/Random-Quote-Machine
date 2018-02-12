c62bcfcc3b1134688273633cdae4a585690d2b5b
Radoq <rpamula92@gmail.com>
2018-02-12 19:45:28 +0100

    es6 update

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ $("#new-quote").on("click", function() {
     $.ajax({
         type: "GET",
         success: function(data) {
-            var quote = $(data).find("#quote");
-            var author = $(data).find("#author");
+            const quote = $(data).find("#quote");
+            const author = $(data).find("#author");
             $("#quote").html(quote);
             $("#author").html(author);
         }
